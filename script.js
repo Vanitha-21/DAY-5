98981d5e95e3a90efa1d8b61eab7f8e7b98a0064
VANITHA <137380423+Vanitha-21@users.noreply.github.com>
2024-01-12 23:46:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let myResume={
+    "basics": {
+      "name": "Vanitha P",
+      "email": "vanitha200302@gamil.com",
+      "phone": 6383033197,
+      "degree": "Bsc Computer Science",
+      "location": {
+        "address": "No:1 Marai Malai Adigalar Street Rajajipuram",
+        "postalCode": 602001,
+        "city": "   Thiruvallur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/Vanitha-21",
+        }
+      ]
+    },
+    "education": [
+
+      {
+        "institution": "Ponnusamy Nadar College Of Arts And Science",
+        "department": "BSc Computer Science",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2023,
+        
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Dancing",
+      }
+    ]
+  }
+  console.log(myResume);
